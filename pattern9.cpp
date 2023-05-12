9d7d5d3e1c4a8e9a34c37048c6c73e1593047e3f
skp544 <shubhamkumarprajapati544@gmail.com>
2023-05-12 22:05:50 +0530

    next-five-patterns

---
+++ pattern9.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter the Number: " << endl;
+    cin >> n;
+
+    cout << "Printing Patterns: " << endl;
+
+    for (int i = 1; i <= n; i++) {
+        int num = i;
+        for (int j = 1; j <= i; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
