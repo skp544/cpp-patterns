1401e5eac86c3a94476107da9ef73c03504ad50d
skp544 <shubhamkumarprajapati544@gmail.com>
2023-05-14 21:42:58 +0530

    next-five-patterns

---
+++ pattern17.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the Number: " << endl;
+    cin >> n;
+
+    cout << "Printing Patterns: " << endl;
+
+    for (int i = 1; i <= n; i++)
+    {
+        char ch = 'A' + i - 1;
+        for (int j = 1; j <= i; j++) {
+            cout << ch;
+            ch++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
